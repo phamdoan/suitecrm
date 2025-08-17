963d19a8bc4608e65d0816148c45d39f771132bd
doanpv <doanpv@mirabo-global.com>
2025-08-17 21:17:51 +0700

    chore: stop tracking vendor

--- vendor/nbgrp/onelogin-saml-bundle/src/Event/UserCreatedEvent.php
+++
@@ -1,8 +0,0 @@
-<?php
-// SPDX-License-Identifier: BSD-3-Clause
-
-declare(strict_types=1);
-
-namespace Nbgrp\OneloginSamlBundle\Event;
-
-class UserCreatedEvent extends AbstractUserEvent {}
--- vendor/nbgrp/onelogin-saml-bundle/src/EventListener/User/UserModifiedListener.php
+++
@@ -1,8 +0,0 @@
-<?php
-// SPDX-License-Identifier: BSD-3-Clause
-
-declare(strict_types=1);
-
-namespace Nbgrp\OneloginSamlBundle\EventListener\User;
-
-class UserModifiedListener extends AbstractUserListener {}
